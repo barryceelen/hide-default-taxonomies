b350c96aabf43c0a9576e4f97ea86debc0f5d1f7
Barry Ceelen <b@rryceelen.com>
2015-02-15 15:43:59 +0100

    Update comments and plugin version

--- hide-default-taxonomies.php
+++ hide-default-taxonomies.php
@@ -2,16 +2,16 @@
 
 /**
  * @package Hide Default Taxonomies
- * @version 0.2
+ * @version 0.3.0
  */
 
-/*
+/**
  * Plugin Name: Hide Default Taxonomies
  * Plugin URI: http://github.com/barryceelen/hide-default-taxonomies
  * Description: (Visually) removes most category and tag functionality from WordPress admin
  * Author: Barry Ceelen
  * Author URI: http://github.com/barryceelen/
- * Version: 0.2
+ * Version: 0.3.0
  * License: GPL2+
  */
 
@@ -28,9 +28,10 @@ class HideDefaultTaxonomies {
 	}
 
 	/**
-	 * Add filters and actions
+	 * Add filters and actions.
+	 *
+	 * @since 0.1.0
 	 */
-
 	private function __construct() {
 
 		add_action( 'admin_enqueue_scripts', array( $this, 'admin_enqueue_scripts' ) );
@@ -42,55 +43,61 @@ class HideDefaultTaxonomies {
 	}
 
 	/**
-	 * Enqueue scripts and/or styles
+	 * Enqueue styles.
+	 *
+	 * @since 0.1.0
 	 */
-
 	public function admin_enqueue_scripts() {
 		wp_enqueue_style( 'hidetax', plugin_dir_url( __FILE__ ) . "/hide-default-taxonomies.css" );
 	}
 
 	/**
-	 * Remove post submenu pages
+	 * Remove post submenu pages.
+	 *
+	 * @since 0.1.0
 	 */
-
 	public function remove_submenu_pages() {
 		remove_submenu_page( 'edit.php', 'edit-tags.php?taxonomy=post_tag' );
 		remove_submenu_page( 'edit.php', 'edit-tags.php?taxonomy=category' );
 	}
 
 	/**
-	 * Remove post meta boxes
+	 * Remove post meta boxes.
+	 *
+	 * @since 0.1.0
 	 */
-
 	public function remove_post_meta_boxes() {
 		remove_meta_box( 'categorydiv',      'post', 'side'   );
 		remove_meta_box( 'tagsdiv-post_tag', 'post', 'side'   );
 	}
 
 	/**
-	 * Unregister sidebar widgets
+	 * Unregister sidebar widgets.
+	 *
+	 * @since 0.1.0
 	 */
-
 	public function unregister_widgets() {
 		unregister_widget( 'WP_Widget_Categories' );
 		unregister_widget( 'WP_Widget_Tag_Cloud' );
 	}
 
 	/**
-	 * Remove nav menu meta boxes
+	 * Remove nav menu meta boxes.
 	 *
-	 * via: http://wordpress.stackexchange.com/questions/97890/how-to-remove-a-metabox-from-menu-editor-page
+	 * @see http://wordpress.stackexchange.com/questions/97890/how-to-remove-a-metabox-from-menu-editor-page
+	 *
+	 * @since 0.1.0
 	 */
-
 	public function remove_post_nav_menu_meta_boxes() {
 	  remove_meta_box('add-category', 'nav-menus', 'side');
 	  remove_meta_box('add-post_tag', 'nav-menus', 'side');
 	}
 
 	/**
-	 * Unset category and tag columns on edit.php
+	 * Unset category and tag columns on edit.php.
+	 *
+	 * @since 0.1.0
 	 */
-
 	public function manage_post_posts_columns( $columns ) {
 		if ( array_key_exists( 'categories', $columns ) )
 			unset( $columns['categories'] );
@@ -98,5 +105,4 @@ class HideDefaultTaxonomies {
 			unset( $columns['tags'] );
 		return $columns;
 	}
-
 }
