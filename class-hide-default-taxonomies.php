888d4cb90a19327968b74bce15ea9bad7769828d
Barry Ceelen <b@rryceelen.com>
2015-02-16 12:08:12 +0100

    Moved plugin class to new file, only include when is_admin

---
+++ class-hide-default-taxonomies.php
@@ -0,0 +1,103 @@
+<?php
+/**
+ * @package Hide Default Taxonomies
+ * @version 0.3.0
+ */
+
+/**
+ * Main plugin class.
+ *
+ * @package Events_Toolkit
+ * @author  Barry Ceelen <b@rryceelen.com>
+ */
+class Hide_Default_Taxonomies {
+
+	public static function get_instance() {
+		static $instance = null;
+		if ( $instance === null ) {
+			$instance = new Hide_Default_Taxonomies();
+		}
+		return $instance;
+	}
+
+	/**
+	 * Add filters and actions.
+	 *
+	 * @since 0.1.0
+	 */
+	private function __construct() {
+
+		add_action( 'admin_enqueue_scripts', array( $this, 'admin_enqueue_scripts' ) );
+		add_action( 'admin_menu', array( $this, 'remove_submenu_pages' ) );
+		add_action( 'add_meta_boxes', array( $this, 'remove_post_meta_boxes' ) );
+		add_action( 'widgets_init', array( $this, 'unregister_widgets' ) );
+		add_action( 'admin_head-nav-menus.php', array( $this, 'remove_nav_menu_meta_boxes' ) );
+		add_filter( 'manage_post_posts_columns', array( $this, 'manage_post_posts_columns' ) );
+	}
+
+	/**
+	 * Enqueue styles.
+	 *
+	 * @since 0.1.0
+	 */
+	public function admin_enqueue_scripts() {
+		wp_enqueue_style( 'hidetax', plugin_dir_url( __FILE__ ) . "/hide-default-taxonomies.css" );
+	}
+
+	/**
+	 * Remove post submenu pages.
+	 *
+	 * @since 0.1.0
+	 */
+	public function remove_submenu_pages() {
+		remove_submenu_page( 'edit.php', 'edit-tags.php?taxonomy=post_tag' );
+		remove_submenu_page( 'edit.php', 'edit-tags.php?taxonomy=category' );
+	}
+
+	/**
+	 * Remove post meta boxes.
+	 *
+	 * @since 0.1.0
+	 */
+	public function remove_post_meta_boxes() {
+		remove_meta_box( 'categorydiv',      'post', 'side'   );
+		remove_meta_box( 'tagsdiv-post_tag', 'post', 'side'   );
+	}
+
+	/**
+	 * Unregister sidebar widgets.
+	 *
+	 * @since 0.1.0
+	 */
+	public function unregister_widgets() {
+		unregister_widget( 'WP_Widget_Categories' );
+		unregister_widget( 'WP_Widget_Tag_Cloud' );
+	}
+
+	/**
+	 * Remove nav menu meta boxes.
+	 *
+	 * @see http://wordpress.stackexchange.com/questions/97890/how-to-remove-a-metabox-from-menu-editor-page
+	 *
+	 * @since 0.1.0
+	 */
+	public function remove_post_nav_menu_meta_boxes() {
+		remove_meta_box('add-category', 'nav-menus', 'side');
+		remove_meta_box('add-post_tag', 'nav-menus', 'side');
+	}
+
+	/**
+	 * Unset category and tag columns on edit.php.
+	 *
+	 * @since 0.1.0
+	 */
+	public function manage_post_posts_columns( $columns ) {
+		if ( array_key_exists( 'categories', $columns ) ) {
+			unset( $columns['categories'] );
+		}
+		if ( array_key_exists( 'tags', $columns ) ) {
+			unset( $columns['tags'] );
+		}
+		return $columns;
+	}
+}
--- hide-default-taxonomies.php
+++ hide-default-taxonomies.php
@@ -8,101 +8,19 @@
 /**
  * Plugin Name: Hide Default Taxonomies
  * Plugin URI: http://github.com/barryceelen/hide-default-taxonomies
- * Description: (Visually) removes most category and tag functionality from WordPress admin
+ * Description: (Visually) removes most category and tag functionality from the WordPress admin
  * Author: Barry Ceelen
  * Author URI: http://github.com/barryceelen/
  * Version: 0.3.0
  * License: GPL2+
  */
 
-add_action( 'plugins_loaded', array( 'HideDefaultTaxonomies', 'get_instance' ) );
+// If this file is called directly, abort.
+if ( ! defined( 'WPINC' ) ) {
+	die;
+}
 
-class HideDefaultTaxonomies {
-
-	public static function get_instance() {
-		static $instance = null;
-		if ( $instance === null ) {
-			$instance = new HideDefaultTaxonomies();
-		}
-		return $instance;
-	}
-
-	/**
-	 * Add filters and actions.
-	 *
-	 * @since 0.1.0
-	 */
-	private function __construct() {
-
-		add_action( 'admin_enqueue_scripts', array( $this, 'admin_enqueue_scripts' ) );
-		add_action( 'admin_menu', array( $this, 'remove_submenu_pages' ) );
-		add_action( 'add_meta_boxes', array( $this, 'remove_post_meta_boxes' ) );
-		add_action( 'widgets_init', array( $this, 'unregister_widgets' ) );
-		add_action( 'admin_head-nav-menus.php', array( $this, 'remove_nav_menu_meta_boxes' ) );
-		add_filter( 'manage_post_posts_columns', array( $this, 'manage_post_posts_columns' ) );
-	}
-
-	/**
-	 * Enqueue styles.
-	 *
-	 * @since 0.1.0
-	 */
-	public function admin_enqueue_scripts() {
-		wp_enqueue_style( 'hidetax', plugin_dir_url( __FILE__ ) . "/hide-default-taxonomies.css" );
-	}
-
-	/**
-	 * Remove post submenu pages.
-	 *
-	 * @since 0.1.0
-	 */
-	public function remove_submenu_pages() {
-		remove_submenu_page( 'edit.php', 'edit-tags.php?taxonomy=post_tag' );
-		remove_submenu_page( 'edit.php', 'edit-tags.php?taxonomy=category' );
-	}
-
-	/**
-	 * Remove post meta boxes.
-	 *
-	 * @since 0.1.0
-	 */
-	public function remove_post_meta_boxes() {
-		remove_meta_box( 'categorydiv',      'post', 'side'   );
-		remove_meta_box( 'tagsdiv-post_tag', 'post', 'side'   );
-	}
-
-	/**
-	 * Unregister sidebar widgets.
-	 *
-	 * @since 0.1.0
-	 */
-	public function unregister_widgets() {
-		unregister_widget( 'WP_Widget_Categories' );
-		unregister_widget( 'WP_Widget_Tag_Cloud' );
-	}
-
-	/**
-	 * Remove nav menu meta boxes.
-	 *
-	 * @see http://wordpress.stackexchange.com/questions/97890/how-to-remove-a-metabox-from-menu-editor-page
-	 *
-	 * @since 0.1.0
-	 */
-	public function remove_post_nav_menu_meta_boxes() {
-	  remove_meta_box('add-category', 'nav-menus', 'side');
-	  remove_meta_box('add-post_tag', 'nav-menus', 'side');
-	}
-
-	/**
-	 * Unset category and tag columns on edit.php.
-	 *
-	 * @since 0.1.0
-	 */
-	public function manage_post_posts_columns( $columns ) {
-		if ( array_key_exists( 'categories', $columns ) )
-			unset( $columns['categories'] );
-		if ( array_key_exists( 'tags', $columns ) )
-			unset( $columns['tags'] );
-		return $columns;
-	}
+if ( is_admin() && ( ! defined( 'DOING_AJAX' ) || ! DOING_AJAX ) ) {
+	require_once( plugin_dir_path( __FILE__ ) . 'class-hide-default-taxonomies.php' );
+	add_action( 'plugins_loaded', array( 'Hide_Default_Taxonomies', 'get_instance' ) );
 }
